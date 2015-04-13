4aa1782ae427ce1cf98f58ad5dccdb7bf8c610dc
weichian <ahoo28303@gmail.com>
2015-04-13 15:40:45 +0800

    iteractive version

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -9,21 +9,21 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
 	push {r3, r4, r5, lr}
-
+        
+	sub r4,r0,#0
+	cmp r0,#0
+	ble .L3
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-        subs r4,r0,#0
-        ble  .L3
+
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
 	cmp r4,#1
 	beq .L4
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
-	add r0,r4,#4294967295
+	sub r0,r4,#1
 	bl fibonacci
 	@ R5 = R0
 	@ R0 = R4 - 2
